6383148b2f1ffbecdf6519b4ce17bcc9a6e0f41d
Avni Agrawal <avani1agrawal@gmail.com>
2020-10-15 11:03:36 +0530

    Added HackerRank codes in cpp

---
+++ max-of-four.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int max(int a,int b);
+int main(){
+    cout<<"enter total numbers";
+    int n;
+    cin>>n;
+    int a[n];
+    for(i=0;i<n;i++)
+{
+   cin>>a[i];
+}
+   max()
+}
+
+int max(int a[i],int b[i+1]){
+  
+  int x=(a>b)?a:b;
+  return (x>max(a[i+2])?)
+}
