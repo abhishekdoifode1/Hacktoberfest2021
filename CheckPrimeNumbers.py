25bc5d7cfaffff63f505a99325e7cd769e4b18a2
AyushTomar1 <73213195+AyushTomar1@users.noreply.github.com>
2020-10-21 11:34:47 +0530

    Update CheckPrimeNumbers.py

--- CheckPrimeNumbers.py
+++ CheckPrimeNumbers.py
@@ -2,19 +2,11 @@
 
 # To take input from the user
 num = int(input("Enter a number: "))
-
-# prime numbers are greater than 1
-if num > 1:
-   # check for factors
-   for i in range(2,num):
-       if (num % i) == 0:
-           print(num,"is not a prime number")
-           print(i,"times",num//i,"is",num)
-           break
-   else:
-       print(num,"is a prime number")
-       
-# if input number is less than
-# or equal to 1, it is not prime
+l=[]
+for i in range(1,num+1):
+   if num%i==0:
+      l.append(i)
+if len(l)==2:
+   print(num,"is a prime number")
 else:
    print(num,"is not a prime number")
