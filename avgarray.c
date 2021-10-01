b89eccf858e775ec07e5de59e28e1916416e7747
bhattanimesh <79653127+bhattanimesh@users.noreply.github.com>
2021-10-01 20:39:31 +0530

    Create avgarray.c

---
+++ avgarray.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+    int n, i;
+    float num[100], sum = 0.0, avg;
+
+    printf("Enter the numbers of elements: ");
+    scanf("%d", &n);
+
+    while (n > 100 || n < 1) {
+        printf("Error! number should in range of (1 to 100).\n");
+        printf("Enter the number again: ");
+        scanf("%d", &n);
+    }
+
+    for (i = 0; i < n; ++i) {
+        printf("%d. Enter number: ", i + 1);
+        scanf("%f", &num[i]);
+        sum += num[i];
+    }
+
+    avg = sum / n;
+    printf("Average = %.2f", avg);
+    return 0;
+}
