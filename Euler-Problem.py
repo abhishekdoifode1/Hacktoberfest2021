19aa461c2d06703114cfbb42961f6c6fc5725f14
Aryaman Pandey <67505482+Aryaman-Pandey187@users.noreply.github.com>
2021-10-02 01:58:22 +0530

    Created a python script for euler problem
    Please review the file and merge accordingly

---
+++ Euler-Problem.py
@@ -0,0 +1,34 @@
+    import os, glob, sys
+    x = 5
+    y = input('fucker')
+    z = eval(y)
+    def dick():
+      while y > x:
+        print("y is bigger")
+        x = x + 1
+
+    dick()		
+    print("The End") 
+    class person:
+        def __init__(self, IQ, PWR):
+            self.IQ = IQ
+            self.PWR = PWR
+
+        def addup(self):
+            return self.IQ + self.PWR
+
+    John = person(127, 50)
+    Luke = person(105, 70)
+    j = John.addup()
+    l = Luke.addup()
+
+    def win():
+        if j > l:
+            print('John wins')
+        elif j == l:
+            print("It's a tie")
+        else:
+            print('Luke wins')
+
+    win()
+    sys.exit()
