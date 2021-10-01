272ea087b2a37338eab8cf01fd67bb6852ce66a7
abhi-hacktober <91714640+abhi-hacktober@users.noreply.github.com>
2021-10-01 10:40:12 +0530

    Update BMI_calculator.c

--- BMI_calculator.c
+++ BMI_calculator.c
@@ -4,7 +4,7 @@ int main() {
 	  double x,y;
 	      printf("Enter your height in meter:");
 		    scanf("%lf",&x);
-		        printf("Enter your weight in kg:");
+		        printf("Enter your weight in KiloGrams (kg):");
 			    scanf("%lf",&y);
 			        double bmi=y/(x*x);
 				    printf("your BMI is : %lf", bmi);
