f3b510db104f5953c94597c151b79c9d136eea38
Isuri Mayadunne <isurimayadunne@gmail.com>
2021-10-01 12:27:16 +0530

    Add a golang fundamental file by Isuri

---
+++ basic_golang.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Hello, I am Jane")
+	fmt.Printf("I am a student")
+
+	fmt.Println(strings.ToUpper("Hello World!"))
+
+	str1 := "Hello"
+	str2 := "World"
+	fmt.Println(str1, str2)
+
+}
