2a0bb3f486f76f545331c72d5a3aa36c7b853c83
ana2407 <74427561+ana2407@users.noreply.github.com>
2021-10-01 14:59:45 +0530

    Create Fibonacci.java

---
+++ java-programs/Fibonacci.java
@@ -0,0 +1,31 @@
+//Code for Fibonacci series in Java contributed by ana2407 for Hacktoberfest 2021
+import java.util.*;
+
+class Fibonacci { 
+
+    static int fib(int n) 
+    {
+        int f[] = new int[n + 2];
+
+        // 0th and 1st number of the series are 0 and 1 
+        f[0] = 0; 
+        f[1] = 1; 
+
+        for (int i = 2; i <= n; i++) {
+            // Add the previous 2 numbers in the series and store it
+            f[i] = f[i - 1] + f[i - 2]; 
+        } 
+
+        for(int i=0;i<n+2;i++){
+            System.out.print(f[i] + " ");
+        }
+    } 
+
+    public static void main(String args[]) 
+    { 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter value of N: ")
+        int n = sc.nextInt();
+        fib(n);
+    } 
+}
