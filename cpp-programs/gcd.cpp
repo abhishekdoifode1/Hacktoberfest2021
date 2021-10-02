359739e54b6ee7626ed0ed4bce4d7d43464050a4
yctseng1227 <gene26620560@gmail.com>
2021-10-02 15:24:09 +0800

    Create gcd.cpp

---
+++ cpp-programs/gcd.cpp
@@ -0,0 +1,16 @@
+// code by yctseng1227
+#include <iostream>
+using namespace std;
+
+int gcd( int a, int b ){
+    if( b==0 )  return a;
+    return gcd( b, a%b );
+}
+
+int main(){
+	int a, b;
+	cin >> a >> b;
+	cout << gcd(a, b);
+	return 0;
+}
+
