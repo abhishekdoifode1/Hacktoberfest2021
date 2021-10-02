47bf8eb7b5c89b98e06900c6e753fbac286e46df
komaljoshi252 <55447267+komaljoshi252@users.noreply.github.com>
2021-10-02 10:18:54 +0530

    Add files via upload

---
+++ java-programs/Kadane'sAlgo.java
@@ -0,0 +1,69 @@
+/* Given an array Arr[] of N integers. Find the contiguous sub-array(containing at least one number) which has the maximum sum and return its sum.
+
+
+Example 1:
+
+Input:
+N = 5
+Arr[] = {1,2,3,-2,5}
+Output:
+9
+Explanation:
+Max subarray sum is 9
+of elements (1, 2, 3, -2, 5) which 
+is a contiguous subarray.*/
+
+import java.io.*;
+
+class Main {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		while(t-->0){
+		    //size of array
+		    int n = Integer.parseInt(br.readLine().trim());
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    
+		    //adding elements
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    
+		    //calling maxSubarraySum() function
+		    System.out.println(obj.maxSubarraySum(arr, n));
+		}
+	}
+}
+
+
+
+
+class Solution{
+
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    int maxSubarraySum(int arr[], int n){
+        
+        // code here
+        
+        int max_ending=0;
+        int max_sum_sofar=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++)
+        {
+             max_ending+= arr[i];
+             if(max_sum_sofar<max_ending) //comparison for the maximum
+             max_sum_sofar=max_ending;
+             if(max_ending<0)
+            max_ending = 0;
+           
+        }
+        return max_sum_sofar;
+    }
+    
+}
+
