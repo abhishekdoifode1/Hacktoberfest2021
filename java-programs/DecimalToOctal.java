0eea00b664c4da949ba7d195b18bb5414debcbaa
nikhil26vaddadi <nikhil26vsn@gmail.com>
2021-10-02 14:51:00 +0530

    Create DecimalToOctal.java

---
+++ java-programs/DecimalToOctal.java
@@ -0,0 +1,8 @@
+public class DecimalToOctal{  
+public static void main(String args[]){  
+//Using the predefined Integer.toOctalString() method  
+//to convert decimal value into octal  
+System.out.println(Integer.toOctalString(8));  
+System.out.println(Integer.toOctalString(19));  
+System.out.println(Integer.toOctalString(81));  
+}}  
