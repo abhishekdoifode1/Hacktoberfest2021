8718885d380fd65545baa231d3167d901b263cc1
SukritSethi <72218366+SukritSethi@users.noreply.github.com>
2021-10-03 19:10:48 +0530

    Create Length_of_string.cpp

---
+++ cpp-programs/Length_of_string.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    char str[] = "Hacktoberfest is awesome";
+
+    cout << "String Length = " << strlen(str);
+
+    return 0;
+}
