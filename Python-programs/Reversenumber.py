906bd731d2debf6206b9f517172ed8e6329e46bf
Murali Ratsa <67418871+muraliratsa@users.noreply.github.com>
2021-10-03 18:34:23 +0530

    Update Reversenumber.py
    Made changes.

--- Python-programs/Reversenumber.py
+++ Python-programs/Reversenumber.py
@@ -1,8 +1,3 @@
-def reverse_for_loop(s):
-s1 = ''
-for c in s:
-s1 = c + s1
-return s1
-my_number = '123456'
 if __name__ == "__main__":
-print('Reversing the given number using for loop =', reverse_for_loop(my_number)
+  num = input("Enter the number: ")
+  print(f"Reverse of the number is {num[::-1]}.") 
