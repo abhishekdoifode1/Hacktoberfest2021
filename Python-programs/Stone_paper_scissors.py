1c1a7883f8ca099b08fee9ca7fe1223519f03142
vipul818 <72666431+vipul818@users.noreply.github.com>
2021-10-03 20:59:34 +0530

    created a new Stone_paper_scissors.py
    I have used tkinter to make this game more interactive.

--- Python-programs/Stone_paper_scissors.py
+++ Python-programs/Stone_paper_scissors.py
@@ -1,67 +1,82 @@
-#Program for an interactive game of "rock paper scissors", player can play as many times as the player wants
-from random import randint
-things=["rock","paper","scissors"] #For the choices of AI
-thingsP={"R":"rock","P":"paper","S":"scissors"} #For the choices of player , so that player,s choice can be displayed , makes game more interactive
-a="Y"
-P=0
-C=0
-while(a=="Y" or a=="y"):
-    ch=input("Enter R for rock , P for paper and S for scissors:")
-    ch=ch.upper()  #So that the game is not case-sensitive , will kill all the fun!
-    pch=thingsP[ch]
-    chAI=things[randint(0,2)] #Choice of the AI
-    print("You chose",pch,"and AI chose",chAI) #Showing the choices of both the player and game
-    if(chAI):
-        if(ch=="R"): 
-            if(chAI=="paper"):
-                P-=1
-                c=+1
-                print("Oh no!!!You lost to AI!")
-                print("AI's score:",C)
-            elif(chAI=="scissors"):
-                P+=1
-                c=-1
-                print("Yes!!!You beat the AI!")
-                print("Your score:",P)
-                print("AI's score:",C)
-            else:
-                print("Nothing happens")
-                print("Your score:",P)
-                print("AI's score:",C)
-        elif(ch=="P"):
-            if(chAI=="scissors"):
-                P-=1
-                c=+1
-                print("Oh no!!!You lost to AI!")
-                print("Your score:",P)
-                print("AI's score:",C)
-            elif(chAI=="rock"):
-                P+=1
-                c=-1
-                print("Yes!!!You beat the AI!")
-                print("Your score:",P)
-                print("AI's score:",C)
-            else:
-                print("Nothing happens")
-                print("Your score:",P)
-                print("AI's score:",C)
-        elif(ch=="S"):
-            if(chAI=="rock"):
-                P-=1
-                c=+1
-                print("Oh no!!!You lost to AI!")
-                print("Your score:",P)
-                print("AI's score:",C)
-            elif(chAI=="paper"):
-                P+=1
-                c=-1
-                print("Yes!!!You beat the AI!")
-                print("Your score:",P)
-                print("AI's score:",C)
-            else:
-                print("Nothing happens")
-                print("Your score:",P)
-                print("AI's score:",C)
-    a=input("Play Again? Press n/N for no...else press Y/y  to continue:")  #Provides the choice for the player to play again as many times as they want
+#using tkinter to make a stone, paper, scissors game against computer
+#import library
+from tkinter import *
+import random
+
+#initialize window
+root = Tk()
+root.geometry('500x400')
+root.resizable(0,0)
+root.title('stone,Paper,Scissors')
+root.config(bg ='seashell3')
+
+
+#heading
+Label(root, text = 'stone, Paper ,Scissors' , font='arial 20 bold', bg = 'seashell2').pack()
+
+
+##user choice
+user_take = StringVar()
+Label(root, text = 'choose any one: stone, paper ,scissors' , font='arial 15 bold', bg = 'seashell2').place(x = 20,y=70)
+Entry(root, font = 'arial 15', textvariable = user_take , bg = 'antiquewhite2').place(x=90 , y = 130)
+
+
+
+#computer choice
+comp_pick = random.randint(1,3)
+if comp_pick == 1:
+    comp_pick = 'stone'
+elif comp_pick ==2:
+    comp_pick = 'paper'
+else:
+    comp_pick = 'scissors'
+    
+
+
+
+##function to play
+Result = StringVar()
+
+def play():
+    user_pick = user_take.get()
+    if user_pick == comp_pick:
+        Result.set('tie,you both select same')
+    elif user_pick == 'stone' and comp_pick == 'paper':
+        Result.set('you loose,computer select paper')
+    elif user_pick == 'stone' and comp_pick == 'scissors':
+        Result.set('you win,computer select scissors')
+    elif user_pick == 'paper' and comp_pick == 'scissors':
+        Result.set('you loose,computer select scissors')
+    elif user_pick == 'paper' and comp_pick == 'stone':
+        Result.set('you win,computer select stone')
+    elif user_pick == 'scissors' and comp_pick == 'stone':
+        Result.set('you loose,computer select stone')
+    elif user_pick == 'scissors' and comp_pick == 'paper':
+        Result.set('you win ,computer select paper')
+    else:
+        Result.set('invalid: choose any one -- stone, paper, scissors')
+    
+        
+    
+##fun to reset
+def Reset():
+    Result.set("") 
+    user_take.set("")
+
+##fun to exit
+def Exit():
+    root.destroy()
+
+
+###### button
+Entry(root, font = 'arial 10 bold', textvariable = Result, bg ='antiquewhite2',width = 50,).place(x=25, y = 250)
+
+Button(root, font = 'arial 13 bold', text = 'PLAY'  ,padx =5,bg ='seashell4' ,command = play).place(x=150,y=190)
+
+Button(root, font = 'arial 13 bold', text = 'RESET'  ,padx =5,bg ='seashell4' ,command = Reset).place(x=70,y=310)
+
+Button(root, font = 'arial 13 bold', text = 'EXIT'  ,padx =5,bg ='seashell4' ,command = Exit).place(x=230,y=310)
+
+root.mainloop()
     
 
