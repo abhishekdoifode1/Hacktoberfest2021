2a185ed3f85c2cfb5d480782ef15c7612f54b6b3
jaytenenbaum <37470864+jaytenenbaum@users.noreply.github.com>
2021-10-09 12:43:37 +0300

    Refactor Count_Vowels.py to match pep8

--- Python-programs/Count_Vowels.py
+++ Python-programs/Count_Vowels.py
@@ -9,11 +9,11 @@ ip_str = 'Hello, have you tried our tutorial section yet?'
 ip_str = ip_str.casefold()
 
 # make a dictionary with each vowel a key and value 0
-count = {}.fromkeys(vowels,0)
+count = {}.fromkeys(vowels, 0)
 
 # count the vowels
 for char in ip_str:
-   if char in count:
-       count[char] += 1
+    if char in count:
+        count[char] += 1
 
-print(count)
+print count
