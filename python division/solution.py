dc8c744a24532130c6f449bc47246c311ee20511
senurifernando <senuri0604@gmail.com>
2021-10-13 12:01:25 +0530

    python: division (author; senuri fernando)

---
+++ python division/solution.py
@@ -0,0 +1,7 @@
+# author: Senuri Fernando
+a = int(input())  # user input taken
+b = int(input())  # user input taken
+result1 = int(a/b)  # result converted to integer
+print(result1)
+result2 = float(a/b)  # result converted to integer
+print(result2)
